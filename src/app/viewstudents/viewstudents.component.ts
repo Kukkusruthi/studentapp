4d7d774ff393672cafd6c7f079b0a94b25113814
kukkusruthi <nsruthi054@gmail.com>
2022-07-08 11:59:44 +0530

    created

--- src/app/viewstudents/viewstudents.component.ts
+++ src/app/viewstudents/viewstudents.component.ts
@@ -12,5 +12,15 @@ export class ViewstudentsComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  name="Akhil"
+  data=[
+    {"name":"sruthi","age":20,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3F3e5C2_4KVWpSSvmBDVb8iPGyRnDB5DVPA&usqp=CAU"},
+    {"name":"malu","age":5,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3F3e5C2_4KVWpSSvmBDVb8iPGyRnDB5DVPA&usqp=CAU"},
+    {"name":"diya","age":4,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3F3e5C2_4KVWpSSvmBDVb8iPGyRnDB5DVPA&usqp=CAU"},
+    {"name":"minnu","age":23,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3F3e5C2_4KVWpSSvmBDVb8iPGyRnDB5DVPA&usqp=CAU"},
+    {"name":"minna","age":56,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3F3e5C2_4KVWpSSvmBDVb8iPGyRnDB5DVPA&usqp=CAU"},
+    {"name":"keerthi","age":34,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3F3e5C2_4KVWpSSvmBDVb8iPGyRnDB5DVPA&usqp=CAU"}
+  ]
+    
+
+  
 }
